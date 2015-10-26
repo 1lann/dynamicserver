ca2aca5645d1943e2b5d70646508a40ddcdcd864
Jason Chu <jasonchujc@gmail.com>
2015-10-26 23:59:39 +0800

    Move state and beacon code into beacon.go

--- reverse_proxy/beacon.go
+++ reverse_proxy/beacon.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/1lann/beacon/chat"
 	"github.com/1lann/beacon/handler"
 	"io"
 	"log"
@@ -8,6 +9,117 @@ import (
 	"strings"
 	"sync"
 )
+
+type state int
+
+var currentState state
+
+const (
+	stateInitializing = iota
+	stateStopped
+	stateIdle
+	stateIdling
+	stateStarted
+	stateStarting
+	stateUnavailable
+)
+
+const prefixText = chat.Aqua + "[chuie.io] " + chat.White
+const headerText = chat.Aqua + "-- [ chuie.io ] --\n\n" + chat.White
+
+func (s state) String() string {
+	switch s {
+	case stateInitializing:
+		return "Intializing"
+	case stateStopped:
+		return "Stopped"
+	case stateIdle:
+		return "Idle"
+	case stateIdling:
+		return "Idling"
+	case stateStarted:
+		return "Started"
+	case stateStarting:
+		return "Starting"
+	}
+
+	return "Unknown"
+}
+
+var connectMessage string
+
+func setState(s state) {
+	// if time.Now().Sub(lastSet) < time.Duration(time.Second*2) {
+	// 	log.Println("Set state cancelled due to being too fast")
+	// }
+
+	// lastSet := time.Now()
+
+	if s != stateStarted && currentState == stateStarted {
+		stopForwarder()
+		go startBeacon()
+	}
+
+	switch s {
+	case stateInitializing:
+		connectMessage = "Sorry, the server is not ready take requests yet! " +
+			"Wait and try again later."
+		handler.OnConnect = onConnectMessage
+		handler.CurrentStatus.Message = prefixText +
+			chat.Yellow + "Initializing..."
+		handler.CurrentStatus.ShowConnection = false
+	case stateStopped:
+		connectMessage = "Sorry, the server is intentionally down.\n" +
+			"Contact the server owner for more information."
+		handler.OnConnect = onConnectMessage
+		handler.CurrentStatus.Message = prefixText +
+			chat.Red + "Intentionally down"
+		handler.CurrentStatus.ShowConnection = false
+	case stateIdling:
+		connectMessage = "Sorry, the server is currently shutting down. " +
+			"You may start it again when it is completely turned off.\n" +
+			"Try connecting again in a few minutes."
+		handler.OnConnect = onConnectMessage
+		handler.CurrentStatus.Message = prefixText + chat.Yellow +
+			"Shutting down..."
+		handler.CurrentStatus.ShowConnection = false
+	case stateIdle:
+		handler.OnConnect = onConnectIdle
+		handler.CurrentStatus.Message = "Off"
+		handler.CurrentStatus.ShowConnection = true
+	case stateStarting:
+		connectMessage = "Sorry, the server is still starting up. " +
+			"Try again in a minute."
+		handler.OnConnect = onConnectMessage
+		handler.CurrentStatus.Message = prefixText + chat.LightGreen +
+			"Starting up..."
+		handler.CurrentStatus.ShowConnection = false
+	case stateUnavailable:
+		connectMessage = "The server is unavailable due to an error. " +
+			"Contact the server owner for help."
+		handler.OnConnect = onConnectMessage
+		handler.CurrentStatus.Message = prefixText + chat.Red + "Unavailable"
+		handler.CurrentStatus.ShowConnection = false
+	case stateStarted:
+		if currentState != stateStarted {
+			stopBeacon()
+			go startForwarder()
+		}
+	}
+
+	currentState = s
+}
+
+func onConnectMessage(player *handler.Player) string {
+	return headerText + connectMessage
+}
+
+func onConnectIdle(player *handler.Player) string {
+	log.Println(player.Username + " has started the server.")
+	go restoreServer()
+	return headerText +
+		"The server is now starting. Come back in about 5 minutes."
+}
 
 var forwardListener net.Listener
 var forwardAddr string
